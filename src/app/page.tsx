c7c1050d2238ea3ecefc7d65622d2f04e8f654cd
Jake DiBattista <Jake.a.dibattista@gmail.com>
2026-01-03 10:21:07 -0500

    Remove white container around iPhone in 'Try it yourself' section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -211,9 +211,7 @@ export default function Home() {
 
             <div className="mx-auto w-full max-w-xl">
               <ScrollAnimation delay={0.1} direction="right">
-                <div className="rounded-3xl border border-black/5 bg-white/35 p-4 sm:p-6 shadow-xl backdrop-blur">
-                  <InteractiveDemo />
-                </div>
+                <InteractiveDemo />
               </ScrollAnimation>
             </div>
           </div>
